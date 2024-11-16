f2485e7647ab2627df816a46db3e935be9d2b55a
Beriso Beker <bekerberiso@gmail.com>
2024-11-16 17:17:06 +0300

    implement first pages create in this branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import SignIn from "./pages/Signin"
 import SignUp from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 
 export default function  App() {
   return (
 
     
 <BrowserRouter>
+<Header />
 <Routes>
 
   <Route path="/" element={<Home />} />
